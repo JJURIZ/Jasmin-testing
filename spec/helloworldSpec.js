f075481eddfaf8be92e79bcc1627f2f17bdfd997
Jeremy Uriz <jeremyuriz@Jennifers-Air.attlocal.net>
2020-11-20 17:48:51 -0500

    init commit

---
+++ spec/helloworldSpec.js
@@ -0,0 +1,7 @@
+describe('Hello World', function() {
+    // const printThis = require('../lib/helloworld')
+    it('should return hello world', function() {
+        let result = printThis(); // Hello world
+        expect(result).toEqual('Hello World')
+    })
+})
